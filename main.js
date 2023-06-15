c5f3b1e000ab9c05ca99ea422856f10e8af58e95
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2023-06-15 13:22:25 -0600

    added text message and CSS

--- main.js
+++ main.js
@@ -2,18 +2,22 @@ document.querySelector('#check').addEventListener('click', check)
 
 function check() {
     const day = document.querySelector('#day').value.toLowerCase()
-    
+
     if (day === 'tuesday' || day === 'thursday') {
-        message = 'You have class today'; 
+        message = 'You have class today!';
+        document.querySelector('#placeToSee').style.color = 'red'
     } else if (day === 'saturday' || day === 'sunday') {
         message = 'It\'s the weekend!'
     } else if (day === 'monday' || day === 'wednesday') {
-        message = 'These days are soooo long...';        
+        document.querySelector('#placeToSee').style.color = 'gray'
+        message = 'These days are soooo long...';
     } else {
         message = 'Take the day off!'
+        document.querySelector('#placeToSee').style.color = 'purple'
     }
+    
+    document.querySelector('h2').innerText = message
+    document.querySelector('#placeToSee').style.fontSize = '54px'
 
     document.querySelector('#placeToSee').innerText = message
-    document.querySelector('#placeToSee').style.color = 'red'
-    document.querySelector('#placeToSee').style.fontSize = '54px'
 }
